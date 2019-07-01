6e8fc1048a8e746916ddb4ad6ca64ab439a6863a
Otso Hellman <otso.hellman@gmail.com>
2019-07-01 14:05:22 +0300

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
   return (
@@ -20,7 +18,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
