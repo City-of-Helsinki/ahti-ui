3cbae9fb6423b26a5788550c3ddf189230c7742e
Otso Hellman <otso.hellman@gmail.com>
2019-07-08 11:08:34 +0300

    add firebase???

--- src/Containers/App.js/App.js
+++ src/Containers/App.js/App.js
@@ -1,31 +1,25 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import MapGL, { Marker, Popup } from 'react-map-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
 import { accessToken } from '../../config'
 import styles from './App.module.css'
-import asd from '../../resources/style.json'
-import { fromJS } from 'immutable';
-import MAP_STYLE from '../../resources/defaultMapStyle.json'
 import firebase from '../../services/firebase'
-import CityPin from '../city-pin';
+import CityPin from '../city-pin'
+
+const defaultViewport = {
+  latitude: 60.15,
+  longitude: 24.944,
+  zoom: 10,
+  bearing: 0,
+  pitch: 0,
+  minzoom: 3,
+  maxzoom: 9,
+}
 
 export default () => {
-
-  const [viewport, setViewport] = useState({
-    latitude: 60.15,
-    longitude: 24.944,
-    zoom: 10,
-    bearing: 0,
-    pitch: 0,
-    minzoom: 3,
-    maxzoom: 9,
-  })
-
-  const [map, setMap] = useState({})
-  const [routeFilter, setRouteFilter] = useState(['!has', 'start_id'])
-  const [routeData, setRouteData] = useState([])
+  const map = useRef(null)
+  const [viewport, setViewport] = useState(defaultViewport)
   const [popupInfo, setPopupInfo] = useState(null)
-  const [markerInfo, setMarkerInfo] = useState(null)
   const [pointData, setPointData] = useState([])
 
   useEffect(() => {
@@ -34,68 +28,7 @@ export default () => {
     })
   }, [])
 
-
-  const toggleFilter = (start_id) => {
-
-    const currentFilter = map.getFilter('testi')
-    map.setFilter('testi', ['==', 'start_id', start_id])
-    if (currentFilter) {
-      console.log(currentFilter)
-      console.log(routeFilter)
-      console.log(currentFilter == routeFilter)
-      map.setFilter('testi', ['==', 'start_id', start_id])
-      map.setFilter('testi', routeFilter)
-      
-    }
-  }
-
-  const _onClick = event => {
-    console.log("moro")
-    const feature = event.features && event.features[0]
-
-    if (feature) {
-
-
-      if (feature.layer.id === "turvalaitteet-0-94xdva") {
-        const { NIMIS } = feature.properties
-        const longitude = feature.geometry.coordinates[0]
-        const latitude = feature.geometry.coordinates[1]
-        setPopupInfo({
-          longitude,
-          latitude,
-          NIMIS
-        })
-      }
-    }
-  };
-
-  const _markerOnCLick = event => {
-    console.log(event)
-    const feature = event.features && event.features[0]
-
-  }
-
   const _renderPopup = () => {
-    return (
-      popupInfo && (
-        <Popup
-          tipSize={5}
-          anchor="top"
-          longitude={markerInfo.longitude}
-          latitude={markerInfo.latitude}
-          closeOnClick={true}
-          onClose={() => setMarkerInfo(null)}
-        >
-          <div>
-            <p>Tää on popup</p>
-            <p>moromoro</p>
-          </div>
-        </Popup>
-      )
-    );
-  }
-
-  const _renderMarker = () => {
     return (
       popupInfo && (
         <Popup
@@ -104,51 +37,44 @@ export default () => {
           longitude={popupInfo.longitude}
           latitude={popupInfo.latitude}
           closeOnClick={true}
+          closeButton={false}
           onClose={() => setPopupInfo(null)}
         >
           <div>
             <p>Tää on popup</p>
-            <p>{popupInfo.NIMIS}</p>
+            <p>{popupInfo.text}</p>
           </div>
         </Popup>
       )
-    );
+    )
   }
+
+  const _markerOnClick = point => {
+    const { longitude, latitude } = point.location
+    setPopupInfo({
+      longitude,
+      latitude,
+      text: point.name
+    })
+  }
+
   return (
     <div className={styles.container}>
       <MapGL
-        onLoad={(mapObject) => {
-          //console.log(map.target.getStyle().layers.filter(layer => layer.id === "testi")[0])
-          //console.log(map.target.queryRenderedFeatures({ layers: ["testi"] }))
-          //setRouteData(map.target.queryRenderedFeatures({ layers: ["testi"] }))
-          setMap(mapObject.target)
-        }}
+        ref={map}
         {...viewport}
-        mapStyle="mapbox://styles/ohel/cjxodqmm92eao1cqnjz85qnww"//{asd}//mapbox://styles/ohel/cjxlhu6ec0ovd1dlmhs284s73"
+        mapStyle="mapbox://styles/ohel/cjxodqmm92eao1cqnjz85qnww"
         mapboxApiAccessToken={accessToken}
         width="100%"
         height="100%"
         onViewportChange={(viewport) => setViewport(viewport)}
         clickRadius={2}
-        onClick={_onClick}
       >
         {_renderPopup()}
-        {_renderMarker()}
 
         {pointData.map((point, index) =>
           <Marker key={`marker-${index}`} longitude={point.location.longitude} latitude={point.location.latitude}>
-            {/* <CityPin size={50} onClick={() => console.log(point.name)}/> */}
-            <img
-              src="https://images.unsplash.com/photo-1562047330-8c629310c0d7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2167&q=80"
-              style={{
-                width: "50px",
-                height: "50px"
-              }}
-              onClick={() => {
-                console.log(point.name)
-                point.name === "Minna" ? toggleFilter(1) : toggleFilter(4)
-              }}
-            />
+            <CityPin size={50} onClick={() => _markerOnClick(point)} />
           </Marker>
         )}
       </MapGL>
