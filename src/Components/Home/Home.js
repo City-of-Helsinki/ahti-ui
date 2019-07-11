33f1b75b0eb8a1e56a6f383e19f8c88c52eb984b
Margarita Obraztsova <marharyta.obraztsova@gmail.com>
2019-07-12 00:07:30 +0300

    fix: Only display slider in Home if data is loaded

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -15,33 +15,34 @@ const sliderSettings = {
 };
 
 export default ({ pointData }) => {
+  const contextGeoData = useContext(GlobalGeoContext);
   return (
     <div>
       <Header as="h1">Discover</Header>
-      <Slider {...sliderSettings}>
-        {useContext(GlobalGeoContext) &&
-          [
-            ...new Set(
-              useContext(GlobalGeoContext).map(point => point.properties.type)
-            ),
-          ].map((point, id) => (
-            <Link
-              to={{
-                pathname: '/map',
-                search: `?collection=${point}`,
-              }}
-              key={id}
-            >
-              <Header as="h3" className="collection-header">
-                {point}
-              </Header>
-              <img
-                src="https://images.unsplash.com/photo-1562670652-e5947bddb335?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80"
-                alt="moroQ"
-              />
-            </Link>
-          ))}
-      </Slider>
+
+      {contextGeoData && (
+        <Slider {...sliderSettings}>
+          {[...new Set(contextGeoData.map(point => point.properties.type))].map(
+            (point, id) => (
+              <Link
+                to={{
+                  pathname: '/map',
+                  search: `?collection=${point}`,
+                }}
+                key={id}
+              >
+                <Header as="h3" className="collection-header">
+                  {point}
+                </Header>
+                <img
+                  src="https://images.unsplash.com/photo-1562670652-e5947bddb335?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80"
+                  alt="moroQ"
+                />
+              </Link>
+            )
+          )}
+        </Slider>
+      )}
     </div>
   );
 };
