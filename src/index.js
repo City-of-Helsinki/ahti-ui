4faaa0bfc0c6ba1634ede2fbd01f20274c40c2c7
Margarita Obraztsova <marharyta.obraztsova@gmail.com>
2019-07-22 13:53:11 +0300

    ci: Re-add service worker from create-react app

--- src/index.js
+++ src/index.js
@@ -4,27 +4,11 @@ import App from './App';
 import './globalStyles/index.scss';
 
 import i18n from './i18n'; // this needs to be here for i18n to work
-// import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('./serviceWorker.js').then(
-      function(registration) {
-        // Registration was successful
-        console.log(
-          'ServiceWorker registration successful with scope: ',
-          registration.scope
-        );
-      },
-      function(err) {
-        // registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
-      }
-    );
-  });
-}
+serviceWorker.register();
