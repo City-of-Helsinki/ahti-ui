2073effc0b0fd431a8a2ebbf23c328125d646eb4
Otso Hellman <otso.hellman@gmail.com>
2019-07-22 11:29:04 +0300

    fix: naming

--- src/Components/TypeTitle/TypeTitle.js
+++ src/Components/TypeTitle/TypeTitle.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-const SecondaryTitle = styled.h2`
+const TypeTitle = styled.h2`
   font-size: 1.3rem;
   display: block;
   font-family: ${props => props.theme.fonts.fontFamilyBold};
@@ -10,4 +10,4 @@ const SecondaryTitle = styled.h2`
   opacity: 0.7;
 `;
 
-export default SecondaryTitle;
+export default TypeTitle;
