0be2087b0c09896240a3e4693f95693d02c5b232
fpapado <fotis@futurice.com>
2019-07-30 17:21:08 +0300

    feat: Put menu button inside DropDown

--- src/Components/Dropdown/Dropdown.js
+++ src/Components/Dropdown/Dropdown.js
@@ -3,22 +3,21 @@ import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import BodyText from '../BodyText/BodyText';
 import BaseButton from '../BaseButton/BaseButton';
-
+import { ReactComponent as Menu } from '../../assets/icons/menu.svg';
 import { ReactComponent as Exit } from '../../assets/icons/exit.svg';
 
 import styled, { keyframes } from 'styled-components';
 
 const fadeIn = keyframes`
   from {
-  opacity: 0;
+    opacity: 0;
   }
   to {
-  opacity: 1;
+    opacity: 1;
   }
 `;
 
 const Container = styled.div`
-  animation: ${fadeIn} 0.4s;
   z-index: 5000;
   position: absolute;
 
@@ -28,7 +27,6 @@ const Container = styled.div`
 
 const Body = styled.div`
   background-color: ${props => props.theme.colors.white};
-  position: absolute;
   height: 80vh;
   width: 100%;
   padding-top: 1rem;
@@ -43,21 +41,16 @@ const TextSection = styled.section`
   padding: 1.5rem 2rem;
 `;
 
-const ExitButton = styled(BaseButton)`
-  z-index: 10000;
-  top: 2rem;
-  right: 2rem;
-  position: absolute;
-`;
-
 const Line = styled.hr`
   margin: 0;
 `;
 
-const BackgroundShade = styled.div`
+const BackDrop = styled.div`
+  display: ${props => !props.isOpen && 'none'};
+  /* Play the animation when the display changes from none to initial */
+  animation: ${fadeIn} 0.4s ease-in-out;
   position: absolute;
-  top: 80vh;
-  height: 20vh;
+  height: 100vh;
   width: 100%;
   z-index: 4999;
   background-color: rgb(0, 0, 0, 0.3);
@@ -69,7 +62,25 @@ const LanguageButton = styled(BaseButton)`
   font-weight: 600;
 `;
 
-const Dropdown = ({ onExit }) => {
+const MenuButton = styled(BaseButton)`
+  z-index: 5001;
+  position: absolute;
+  top: 1rem;
+  right: 2rem;
+
+  background-color: ${props => props.theme.colors.white};
+  box-shadow: 2px 4px 8px 2px rgba(0, 0, 0, 0.15);
+  border-radius: 50%;
+  width: 5rem;
+  height: 5rem;
+
+  &:focus {
+    outline: 4px solid transparent;
+    box-shadow: 0 0 0 4px rgba(0, 0, 0, 0.4);
+  }
+`;
+
+const Dropdown = ({ isOpen, onOpen, onClose }) => {
   const { t, i18n } = useTranslation();
 
   const onLanguageChange = () => {
@@ -80,29 +91,34 @@ const Dropdown = ({ onExit }) => {
 
   return (
     <Container>
-      <ExitButton onClick={onExit}>
-        <Exit />
-      </ExitButton>
-      <Body>
-        <LanguageButton onClick={onLanguageChange}>
-          {t('dropdown.change_language')}
-        </LanguageButton>
-        <TextSection>
-          <Link to="/">
-            <BodyText>{t('dropdown.add_location')}</BodyText>
-          </Link>
-          <Link to="/">
-            <BodyText>{t('dropdown.give_feedback')}</BodyText>
-          </Link>
-        </TextSection>
-        <Line />
-        <TextSection>
-          <Link to="/">
-            <BodyText>{t('dropdown.about')}</BodyText>
-          </Link>
-        </TextSection>
-      </Body>
-      <BackgroundShade />
+      <MenuButton
+        onClick={isOpen ? onClose : onOpen}
+        aria-expanded={isOpen}
+        aria-label={t('dropdown.label')}
+      >
+        {isOpen ? <Exit /> : <Menu />}
+      </MenuButton>
+      <BackDrop isOpen={isOpen}>
+        <Body>
+          <LanguageButton onClick={onLanguageChange}>
+            {t('dropdown.change_language')}
+          </LanguageButton>
+          <TextSection>
+            <Link to="/">
+              <BodyText>{t('dropdown.add_location')}</BodyText>
+            </Link>
+            <Link to="/">
+              <BodyText>{t('dropdown.give_feedback')}</BodyText>
+            </Link>
+          </TextSection>
+          <Line />
+          <TextSection>
+            <Link to="/">
+              <BodyText>{t('dropdown.about')}</BodyText>
+            </Link>
+          </TextSection>
+        </Body>
+      </BackDrop>
     </Container>
   );
 };
--- src/Components/DropdownContainer/DropdownContainer.js
+++ src/Components/DropdownContainer/DropdownContainer.js
@@ -1,9 +1,7 @@
 import React, { useState } from 'react';
 import { ReactComponent as AhtiLogo } from '../../assets/icons/ahti_logo.svg';
-import { ReactComponent as MenuLogo } from '../../assets/icons/menu.svg';
-import Dropdown from '../Dropdown/Dropdown';
+import DropdownMenu from '../Dropdown/Dropdown';
 import UnstyledLink from '../UnstyledLink/UnstyledLink';
-import BaseButton from '../BaseButton/BaseButton';
 import { useTranslation } from 'react-i18next';
 
 import styled from 'styled-components';
@@ -20,53 +18,32 @@ const TitleContainer = styled.div`
   padding: 1.5rem;
 `;
 
-const MenuButton = styled(BaseButton)`
-  z-index: 2000;
-  position: absolute;
-  top: 1rem;
-  right: 2rem;
-
-  background-color: ${props => props.theme.colors.white};
-  box-shadow: 2px 4px 8px 2px rgba(0, 0, 0, 0.15);
-  border-radius: 50%;
-  width: 5rem;
-  height: 5rem;
-`;
-
+// TODO: Rename to Navigation
 const DropdownContainer = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   const { t, i18n } = useTranslation();
   return (
-    <React.Fragment>
-      {menuOpen && (
-        <Dropdown
-          onExit={() => {
-            document.body.style.overflowY = 'scroll';
-            setMenuOpen(false);
-          }}
-          onLanguageFI={() => i18n.changeLanguage('fi')}
-          onLanguageEN={() => i18n.changeLanguage('en')}
-        />
-      )}
-      {!menuOpen && (
-        <React.Fragment>
-          <UnstyledLink to="/">
-            <TitleContainer>
-              <AhtiLogo />
-            </TitleContainer>
-          </UnstyledLink>
-          <MenuButton
-            onClick={() => {
-              document.body.style.overflowY = 'hidden';
-              setMenuOpen(true);
-            }}
-          >
-            <MenuLogo />
-          </MenuButton>
-        </React.Fragment>
-      )}
-    </React.Fragment>
+    <nav>
+      <UnstyledLink to="/">
+        <TitleContainer>
+          <AhtiLogo />
+        </TitleContainer>
+      </UnstyledLink>
+      <DropdownMenu
+        isOpen={menuOpen}
+        onOpen={() => {
+          document.body.style.overflowY = 'hidden';
+          setMenuOpen(true);
+        }}
+        onClose={() => {
+          document.body.style.overflowY = 'scroll';
+          setMenuOpen(false);
+        }}
+        onLanguageFI={() => i18n.changeLanguage('fi')}
+        onLanguageEN={() => i18n.changeLanguage('en')}
+      />
+    </nav>
   );
 };
 
