1f51cf23669bfd175a7d2c84ce2f487ca6f245c0
Margarita Obraztsova <marharyta.obraztsova@gmail.com>
2019-08-05 11:05:14 +0300

    fix: Fix router to scroll to top of the page on route change

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import mapData from './mapData.json';
 import lineData from './lineData.json';
 import { ThemeProvider } from 'styled-components';
 import withTracker from './withTracker';
+import ScrollToTop from './ScrollToTop';
 export const GlobalGeoContext = React.createContext();
 export const GlobalLineContext = React.createContext();
 export const POINT_TYPES = [
@@ -64,17 +65,19 @@ class App extends React.Component {
         <GlobalGeoContext.Provider value={mapData.features}>
           <GlobalLineContext.Provider value={lineData.data}>
             <Router>
-              <header>
-                <ApplicationHeader />
-              </header>
+              <ScrollToTop>
+                <header>
+                  <ApplicationHeader />
+                </header>
 
-              {/* NOTE: Make sure to wrap any other Route components withTracker.
-               * An alternative might be to set up a top-level route and only wrap that.
-               */}
-              <Suspense>
-                <Route exact path="/" component={withTracker(Home)} />
-                <Route path="/map" component={withTracker(MapPage)} />
-              </Suspense>
+                {/* NOTE: Make sure to wrap any other Route components withTracker.
+                 * An alternative might be to set up a top-level route and only wrap that.
+                 */}
+                <Suspense>
+                  <Route exact path="/" component={withTracker(Home)} />
+                  <Route path="/map" component={withTracker(MapPage)} />
+                </Suspense>
+              </ScrollToTop>
             </Router>
           </GlobalLineContext.Provider>
         </GlobalGeoContext.Provider>
