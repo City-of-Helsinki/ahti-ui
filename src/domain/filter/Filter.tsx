b5d2300f19441c1d1d0b0d5161daf117f38f1af6
Aleksi Hietanen <aleksi@identio.fi>
2019-11-28 21:01:06 +0200

    feat: Initial implementation of Filter component

---
+++ src/domain/filter/Filter.tsx
@@ -0,0 +1,77 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import styles from './Filter.module.scss';
+import { ReactComponent as Checkmark } from '../../assets/icons/checkmark.svg';
+
+interface FilterOptions<T> {
+  type: string;
+  items: T[];
+}
+
+interface FilterProps<T> {
+  readonly options: FilterOptions<T>[];
+  onShow(selectedFilters: T[]): void;
+  countMatches(selectedFilters: T[]): number;
+}
+
+function Filter<T>({ options, onShow, countMatches }: FilterProps<T>) {
+  const [selected, setSelected] = useState<Set<T>>(new Set());
+  const onToggle = (toggleItem: T) => {
+    const newSelection = new Set(selected);
+    if (selected.has(toggleItem)) {
+      newSelection.delete(toggleItem);
+    } else {
+      newSelection.add(toggleItem);
+    }
+    setSelected(newSelection);
+  };
+  const showButton = () => {
+    const matches = countMatches(Array.from(selected));
+    return (
+      <button
+        className={matches > 0 ? styles.showButton : styles.showButtonDisabled}
+        onClick={() => onShow(Array.from(selected))}
+        disabled={matches === 0}
+      >
+        {matches > 0 ? `Näytä ${matches}` : `Ei osumia`}
+      </button>
+    );
+  };
+  return (
+    <div className={styles.container}>
+      <div className={styles.headerContainer}>
+        <div>{'Suodattimet'}</div>
+        <div>{'Saaret'}</div>
+      </div>
+      <div className={styles.optionContainer}>
+        {options.map((option, id) => (
+          <div key={id}>
+            <div>{option.type}</div>
+            <div>
+              {option.items.map((item, id) => (
+                <button
+                  key={id}
+                  onClick={() => onToggle(item)}
+                  className={selected.has(item) ? styles.activeButton : ''}
+                >
+                  {selected.has(item) && <Checkmark />}
+                  {item}
+                </button>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className={styles.footerContainer}>
+        <button
+          className={styles.clearButton}
+          onClick={() => setSelected(new Set())}
+        >
+          {'Tyhjennä'}
+        </button>
+        {showButton()}
+      </div>
+    </div>
+  );
+}
+
+export default Filter;
