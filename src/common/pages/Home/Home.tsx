d68e622201595b707891451865836cf2f6eb2084
Margarita Obraztsova <margaritaobraztsova@Margaritas-MacBook-Pro.local>
2020-01-29 10:43:00 +0200

    Add UI components

---
+++ src/common/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return <div>whatas dgdjhsfn sd</div>;
+};
+
+const HomeWrapper = () => {
+  return <div> Hello, home</div>;
+};
+
+export { Home, HomeWrapper };
