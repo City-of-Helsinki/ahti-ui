8575d66efedc7dc98168242d77814e5b1a9e66af
Aleksi Hietanen <aleksi@identio.fi>
2020-02-07 13:17:10 +0200

    Add Menu component

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -62,7 +62,28 @@ module.exports = ({ config }) => {
     test: /\.(ts|tsx)$/,
     loader: require.resolve('babel-loader'),
     options: {
-      presets: [['react-app', { flow: false, typescript: true }]]
+      presets: [['react-app', { flow: false, typescript: true }]],
+      customize: require.resolve('babel-preset-react-app/webpack-overrides'),
+
+      plugins: [
+        [
+          require.resolve('babel-plugin-named-asset-import'),
+          {
+            loaderMap: {
+              svg: {
+                ReactComponent: '@svgr/webpack?-svgo,+titleProp,+ref![path]'
+              }
+            }
+          }
+        ]
+      ],
+      // This is a feature of `babel-loader` for webpack (not Babel itself).
+      // It enables caching results in ./node_modules/.cache/babel-loader/
+      // directory for faster rebuilds.
+      cacheDirectory: true,
+      // See #6846 for context on why cacheCompression is disabled
+      cacheCompression: false,
+      compact: false
     }
   });
   config.module.rules.push({
