547022b852466f62640639b8da3a4324f907230d
Margarita Obraztsova <margaritaobraztsova@Margaritas-MacBook-Pro.local>
2020-03-02 14:59:37 +0200

    Redo the map with react-mapbox-gl

--- src/common/ui-components/Map/Map.tsx
+++ src/common/ui-components/Map/Map.tsx
@@ -1,42 +1,27 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactMapboxGl from 'react-mapbox-gl';
-import 'mapbox-gl/dist/mapbox-gl.css';
-// import mapStyle from '../../assets/mapStyle.json';
 import classNames from 'classnames';
 
+import 'mapbox-gl/dist/mapbox-gl.css';
 import styles from './Map.module.scss';
 
-const MapGL = ReactMapboxGl({
-  accessToken:
-    'pk.eyJ1IjoicmlpdHRhZ2lybCIsImEiOiJjamZkdzZ4MXYycnlkMnJudmxwb2x3Yzh0In0.5jCRD5YcqflEAuZqTUct9g'
-});
+export interface Props {
+  className: string;
+}
 
-const state = {
-  viewport: {
-    width: 400,
-    height: 400,
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: [8]
-  }
-};
-
-const Map = ({ className }: { className: string; translate: boolean }) => {
+const Map = ({ className }: Props) => {
   const center = [24.944, 60.15];
+  const style = 'mapbox://styles/mapbox/streets-v9';
 
-  const [viewport, setViewport] = useState({
-    width: 474,
-    height: 520,
-    latitude: 60.15,
-    longitude: 24.944,
-    zoom: 12,
-    minZoom: 8,
-    maxZoom: 18
+  const MapGL = ReactMapboxGl({
+    accessToken:
+      'pk.eyJ1IjoicmlpdHRhZ2lybCIsImEiOiJjamZkdzZ4MXYycnlkMnJudmxwb2x3Yzh0In0.5jCRD5YcqflEAuZqTUct9g'
   });
+
   return (
     <div className={classNames(styles.container, className)}>
       <MapGL
-        style="mapbox://styles/mapbox/streets-v9"
+        style={style}
         center={center}
         containerStyle={{
           height: '100vh',
