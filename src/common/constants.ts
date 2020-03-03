00ea15047c859812215dc5daa5749de5d65fbec2
Aleksi Hietanen <aleksi@identio.fi>
2020-03-03 10:23:41 +0200

    Add Map component

---
+++ src/common/constants.ts
@@ -0,0 +1,3 @@
+export const initialLatitude = 60.15;
+export const initialLongitude = 29.944;
+export const initialZoomLevel = 8;
---
+++ src/common/ui-components/Map/Map.tsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import MapGL, { Marker } from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+import { Feature } from '../../../domain/api/generated/types.d';
+import mapStyle from '../../../assets/mapStyle.json';
+import {
+  initialLatitude,
+  initialLongitude,
+  initialZoomLevel
+} from '../../constants';
+import CategoryIcon from '../CategoryIcon/CategoryIcon';
+
+interface MapProps {
+  readonly className?: string;
+  readonly features: Feature[];
+  onClick(feature: Feature): void;
+}
+
+const getMapStyle = (): {} => {
+  return {
+    ...mapStyle,
+    sprite:
+      window.location.protocol +
+      '//' +
+      window.location.hostname +
+      (window.location.port ? ':' + window.location.port : '') +
+      '/sprites/ahti-sprite'
+  };
+};
+
+const Map: React.FC<MapProps> = ({ className, features, onClick }) => {
+  const [viewPort, setViewPort] = useState({
+    latitude: initialLatitude,
+    longitude: initialLongitude,
+    zoom: initialZoomLevel
+  });
+
+  const renderPin = (feature: Feature, id: number) => {
+    return (
+      <Marker
+        key={id}
+        longitude={feature.geometry.coordinates[0]}
+        latitude={feature.geometry.coordinates[1]}
+      >
+        <div onClick={() => onClick(feature)}>
+          <CategoryIcon category={feature?.properties?.category?.name} />
+        </div>
+      </Marker>
+    );
+  };
+
+  return (
+    <MapGL
+      className={className}
+      width={'100%'}
+      height={'100vh'}
+      {...viewPort}
+      mapStyle={getMapStyle()}
+      onViewportChange={setViewPort}
+    >
+      {features.map((feature: Feature, id: number) => renderPin(feature, id))}
+    </MapGL>
+  );
+};
+
+export default Map;
