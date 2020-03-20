29b48b4418b77afdd8ababfdb336f946b9dd8e91
Margarita Obraztsova <margaritaobraztsova@Margaritas-MacBook-Pro.local>
2020-03-20 13:40:11 +0200

    Hero Banner

---
+++ src/common/ui-components/HeroBanner/HeroBanner.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import classNames from 'classnames/bind';
+import { Koros } from 'hds-react';
+
+import { Video } from '../Video/Video';
+import styles from './HeroBanner.module.scss';
+import ImageWithCard from '../ImageWithCard/ImageWithCard';
+const cx = classNames.bind(styles);
+
+type HeroBannerProps = React.PropsWithChildren<{
+  src: string;
+  hasVideo?: boolean;
+  className?: string;
+}>;
+
+const HeroBanner: React.FC<HeroBannerProps> = ({
+  src,
+  children,
+  hasVideo,
+  className
+}) => {
+  return (
+    <React.Fragment>
+      <ImageWithCard
+        src={'/images/landing.png'}
+        className={classNames(styles.imageWithCard, styles.topImageWithCard)}
+      >
+        <section className={styles.imageWithCardSection}>
+          <h1>This is a header</h1>
+          {/* <Link className={styles.link} to={'/content'}>
+            {t('index.see_all_button')}
+          </Link> */}
+          <Video />
+          <div className={cx(styles.container)}>{children}</div>
+        </section>
+      </ImageWithCard>
+
+      <div className={styles.korosContainer}>
+        <Koros className={styles.koros} />
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default HeroBanner;
