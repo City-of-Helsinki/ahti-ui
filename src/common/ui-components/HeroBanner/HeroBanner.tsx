8b81e1355e2c1275d73883c9494a9ea826e367a5
Margarita Obraztsova <margaritaobraztsova@Margaritas-MacBook-Pro.local>
2020-03-23 15:28:35 +0200

    Add fixes for the video url

--- src/common/ui-components/HeroBanner/HeroBanner.tsx
+++ src/common/ui-components/HeroBanner/HeroBanner.tsx
@@ -5,14 +5,22 @@ import { Koros } from 'hds-react';
 import { Video } from '../Video/Video';
 import styles from './HeroBanner.module.scss';
 import ImageWithCard from '../ImageWithCard/ImageWithCard';
+import videoUrl from '../../../assets/videos/Ahti_vertical.mp4';
+
 const cx = classNames.bind(styles);
 
 type HeroBannerProps = React.PropsWithChildren<{
   src?: string;
   hasVideo?: boolean;
+  url?: string;
 }>;
 
-const HeroBanner: React.FC<HeroBannerProps> = ({ src, children, hasVideo }) => {
+const HeroBanner: React.FC<HeroBannerProps> = ({
+  src,
+  children,
+  hasVideo,
+  url
+}) => {
   return (
     <React.Fragment>
       <ImageWithCard
@@ -20,7 +28,7 @@ const HeroBanner: React.FC<HeroBannerProps> = ({ src, children, hasVideo }) => {
         className={classNames(styles.imageWithCard, styles.topImageWithCard)}
       >
         <section className={styles.imageWithCardSection}>
-          <Video />
+          {videoUrl && <Video url={videoUrl} />}
           <div className={cx(styles.container)}>{children}</div>
         </section>
       </ImageWithCard>
