f20fecb260ca23b894c080f8036c9129c84f2b83
Aleksi Hietanen <aleksi@identio.fi>
2020-03-31 10:52:47 +0300

    Fix icon sizes, add icons to CategoryIcon

--- src/domain/ContentPage/ContentPage.tsx
+++ src/domain/ContentPage/ContentPage.tsx
@@ -16,6 +16,8 @@ import CategoryNavigation from '../../common/ui-components/CategoryNavigation/Ca
 import Spinner from '../../common/ui-components/Spinner/Spinner';
 import spinnerAnimation from '../../common/ui-components/Spinner/animations/spinner_all.json';
 import { availableCategories } from '../constants';
+import { ReactComponent as IconList } from '../../assets/icons/icon-list.svg';
+import { ReactComponent as IconMap } from '../../assets/icons/icon-map.svg';
 
 const cx = classNames.bind(styles);
 
@@ -39,9 +41,9 @@ const ContentPage: React.FC = () => {
           onClose={(ahtiId) => actions.removeFilter(ahtiId)}
         />
         <Toggle
-          onIcon={<IconCheck />}
+          onIcon={<IconMap />}
           onIconText={t('content.map')}
-          offIcon={<IconClose />}
+          offIcon={<IconList />}
           offIconText={t('content.list')}
           toggleState={state.mapViewToggle}
           onToggle={() => actions.toggleMapView()}
