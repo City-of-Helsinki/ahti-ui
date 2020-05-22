8f32f2eb0f602482ffa69fb8375c233577146072
Aleksi Hietanen <aleksi@identio.fi>
2020-05-22 13:23:31 +0300

    Add padding to SuggestionForm content

--- src/domain/SuggestionPage/SuggestionForm.tsx
+++ src/domain/SuggestionPage/SuggestionForm.tsx
@@ -94,111 +94,113 @@ const SuggestionForm: React.FC<SuggestionFormProps> = ({
         return (
           <form onSubmit={handleSubmit} className={styles.form}>
             <Koros flipHorizontal={true} className={styles.koros} />
-            <h2 className={styles.formHeading}>
-              {t('suggestion_form.heading')}
-            </h2>
+            <div className={styles.formContent}>
+              <h2 className={styles.formHeading}>
+                {t('suggestion_form.heading')}
+              </h2>
 
-            <TranslatedFieldInput
-              labelText={t('suggestion_form.name')}
-              id={'name'}
-              value={values.name}
-              handleChange={handleChange}
-              error={errors?.name?.fi}
-            />
+              <TranslatedFieldInput
+                labelText={t('suggestion_form.name')}
+                id={'name'}
+                value={values.name}
+                handleChange={handleChange}
+                error={errors?.name?.fi}
+              />
 
-            <TranslatedFieldInput
-              labelText={t('suggestion_form.description')}
-              id={'description'}
-              value={values.description}
-              handleChange={handleChange}
-              textArea={true}
-              error={errors?.description?.fi}
-            />
+              <TranslatedFieldInput
+                labelText={t('suggestion_form.description')}
+                id={'description'}
+                value={values.description}
+                handleChange={handleChange}
+                textArea={true}
+                error={errors?.description?.fi}
+              />
 
-            <TranslatedFieldInput
-              labelText={t('suggestion_form.short_description')}
-              id={'shortDescription'}
-              value={values.shortDescription}
-              handleChange={handleChange}
-            />
+              <TranslatedFieldInput
+                labelText={t('suggestion_form.short_description')}
+                id={'shortDescription'}
+                value={values.shortDescription}
+                handleChange={handleChange}
+              />
 
-            <Select
-              id="categoryId"
-              value={values.categoryId}
-              options={categories.map((category) => {
-                return {
-                  label: category.name,
-                  value: category.id,
-                };
-              })}
-              onChange={handleChange}
-              required
-              labelText={t('suggestion_form.category')}
-            />
+              <Select
+                id="categoryId"
+                value={values.categoryId}
+                options={categories.map((category) => {
+                  return {
+                    label: category.name,
+                    value: category.id,
+                  };
+                })}
+                onChange={handleChange}
+                required
+                labelText={t('suggestion_form.category')}
+              />
 
-            <TagSelect
-              labelText={t('suggestion_form.tags')}
-              selectedLabelText={t('suggestion_form.selected_tags')}
-              noSelectedTagsText={t('suggestion_form.no_selected_tags')}
-              tags={tags}
-              onSelect={(id) => {
-                const tagIds = new Set(values.tagIds);
-                tagIds.add(id);
-                handleChange({
-                  target: { id: 'tagIds', value: Array.from(tagIds) },
-                });
-              }}
-              onDeselect={(id) => {
-                const tagIds = new Set(values.tagIds);
-                tagIds.delete(id);
-                handleChange({
-                  target: { id: 'tagIds', value: Array.from(tagIds) },
-                });
-              }}
-              selectedTagIds={values.tagIds}
-            />
+              <TagSelect
+                labelText={t('suggestion_form.tags')}
+                selectedLabelText={t('suggestion_form.selected_tags')}
+                noSelectedTagsText={t('suggestion_form.no_selected_tags')}
+                tags={tags}
+                onSelect={(id) => {
+                  const tagIds = new Set(values.tagIds);
+                  tagIds.add(id);
+                  handleChange({
+                    target: { id: 'tagIds', value: Array.from(tagIds) },
+                  });
+                }}
+                onDeselect={(id) => {
+                  const tagIds = new Set(values.tagIds);
+                  tagIds.delete(id);
+                  handleChange({
+                    target: { id: 'tagIds', value: Array.from(tagIds) },
+                  });
+                }}
+                selectedTagIds={values.tagIds}
+              />
 
-            <TextInput
-              labelText={t('suggestion_form.email')}
-              id="email"
-              value={values.email}
-              onChange={handleChange}
-              invalid={!!errors?.email}
-              helperText={errors?.email}
-            />
-            <TextInput
-              labelText={t('suggestion_form.phone')}
-              id="phoneNumber"
-              value={values.phoneNumber}
-              onChange={handleChange}
-            />
-            <TextInput
-              labelText={t('suggestion_form.address')}
-              id="address"
-              value={values.address}
-              onChange={handleChange}
-            />
-            <TextInput
-              labelText={t('suggestion_form.website')}
-              id="website"
-              value={values.website}
-              onChange={handleChange}
-            />
+              <TextInput
+                labelText={t('suggestion_form.email')}
+                id="email"
+                value={values.email}
+                onChange={handleChange}
+                invalid={!!errors?.email}
+                helperText={errors?.email}
+              />
+              <TextInput
+                labelText={t('suggestion_form.phone')}
+                id="phoneNumber"
+                value={values.phoneNumber}
+                onChange={handleChange}
+              />
+              <TextInput
+                labelText={t('suggestion_form.address')}
+                id="address"
+                value={values.address}
+                onChange={handleChange}
+              />
+              <TextInput
+                labelText={t('suggestion_form.website')}
+                id="website"
+                value={values.website}
+                onChange={handleChange}
+              />
 
-            <CoordinateSelect
-              labelText={t('suggestion_form.coordinates')}
-              id="coordinates"
-              value={values.coordinates}
-              handleChange={handleChange}
-            />
+              <CoordinateSelect
+                labelText={t('suggestion_form.coordinates')}
+                id="coordinates"
+                value={values.coordinates}
+                handleChange={handleChange}
+              />
 
-            <div className={styles.formControls}>
-              <Button type="submit">{t('suggestion_form.submit')}</Button>
-              {Object.keys(errors).length !== 0 && (
-                <span className={styles.formError}>
-                  {t('suggestion_form.errors.check_form')}
-                </span>
-              )}
+              <div className={styles.formControls}>
+                <Button type="submit">{t('suggestion_form.submit')}</Button>
+                {Object.keys(errors).length !== 0 && (
+                  <span className={styles.formError}>
+                    {t('suggestion_form.errors.check_form')}
+                  </span>
+                )}
+              </div>
             </div>
           </form>
         );
